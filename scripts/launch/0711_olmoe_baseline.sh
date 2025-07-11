1321e7fd3cbf6ce8071ae650bdfd06db8863e184
wangziqi07 <wangziqi07@baidu.com>
2025-07-12 00:55:18 +0800

    fix mlp_ratio, pad_token_id, add 0711 500M MOE baseline

---
+++ scripts/launch/0711_olmoe_baseline.sh
@@ -0,0 +1,6 @@
+export WANDB_API_KEY=b754941545ab815dc4ca11517cc77f303864915f
+export http_proxy=http://agent.baidu.com:8188
+export https_proxy=http://agent.baidu.com:8188
+MASTER_PORT=29509
+export NCCL_DEBUG=INFO
+torchrun --nproc_per_node=8 --master_port=$MASTER_PORT train.py /root/paddlejob/OLMo_demo/scripts/launch/OLMOE/0711_olmoe_baseline.yaml
